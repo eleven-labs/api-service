026ddd78910ed7e0d17918f8c8d726bf9f72b77c
Thibaut ALGRIN <algrin.thibaut@gmail.com>
2018-04-09 09:36:12 +0200

    Change default key

--- src/Pagination/Provider/HateoasPagination.php
+++ src/Pagination/Provider/HateoasPagination.php
@@ -21,15 +21,15 @@ class HateoasPagination implements PaginationProvider
      *
      * @var array
      */
-    const DEFAULT_PAGINATION_HEADERS = [
+    const DEFAULT_PAGINATION_VALUE = [
         // current page
-        'page' => 'X-Page',
+        'page' => 'page',
         // number of items per page
-        'perPage' => 'X-Per-Page',
+        'perPage' => 'perPage',
         // number of items available in total
-        'totalItems' => 'X-Total-Items',
+        'totalItems' => 'totalPages',
         // number of pages available
-        'totalPages' => 'X-Total-Pages'
+        'totalPages' => 'total'
     ];
 
     /**
@@ -39,7 +39,7 @@ class HateoasPagination implements PaginationProvider
 
     public function __construct(array $config = [])
     {
-        foreach (self::DEFAULT_PAGINATION_HEADERS as $name => $headerName) {
+        foreach (self::DEFAULT_PAGINATION_VALUE as $name => $headerName) {
             if (isset($config[$name])) {
                 $headerName = $config[$name];
             }
@@ -63,11 +63,11 @@ class HateoasPagination implements PaginationProvider
         }
         $count = (int) $data[$this->paginationHeaders['totalPages']];
         $total = (int) $data[$this->paginationHeaders['totalItems']];
-        $perPage = $total === 0 ? 0 : (int) ceil($count/$total);
-        $currentPage = $this->getCurrentPage($data['_links']['self']['href']);
+        $perPage = (int) $data[$this->paginationHeaders['perPage']];
+        $currentPage = (int) $data[$this->paginationHeaders['page']];
         $data = reset($data['_embedded']);
 
-        return new Pagination($currentPage, $count, $total, $perPage, $paginationLinks);
+        return new Pagination($currentPage, $perPage, $total, $count, $paginationLinks);
     }
 
     /**
@@ -81,7 +81,13 @@ class HateoasPagination implements PaginationProvider
      */
     public function supportPagination(array $data, ResponseInterface $response, ResponseDefinition $responseDefinition)
     {
-        if (isset($data['_links']) && isset($data['count']) && isset($data['total']) && isset($data['_embedded'])) {
+        foreach ($this->paginationHeaders as $value) {
+            if (!isset($data[$value])) {
+                return false;
+            }
+        }
+
+        if (isset($data['_links']) && isset($data['_embedded'])) {
             $links = $data['_links'];
             if (isset($links['self']) && isset($links['first']) && isset($links['last'])) {
                 return true;
@@ -104,17 +110,5 @@ class HateoasPagination implements PaginationProvider
         }
 
         return $links;
-    }
-
-    /**
-     * @param string $self
-     * @return int
-     */
-    private function getCurrentPage($self)
-    {
-        $self = parse_url($self);
-        $query = parse_query($self['query']);
-
-        return (int) $query['page'];
     }
 }
